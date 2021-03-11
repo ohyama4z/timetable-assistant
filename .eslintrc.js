fd6a119bc817a6da6302d28ad0325703c52d8236
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-03-11 17:17:45 +0000

    自動整形が効く.eslintrc.jsにする

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier', 'prettier/@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+}
