481b1d4382f7e41f2eb988fe0b8841da606b572a
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-05-04 17:53:36 +0000

    エラーが403から500になりました

--- lib/timetable-assistant-stack.ts
+++ lib/timetable-assistant-stack.ts
@@ -1,54 +1,25 @@
 import * as cdk from '@aws-cdk/core'
 import * as apiGateway from '@aws-cdk/aws-apigateway'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as lambdaNodejs from '@aws-cdk/aws-lambda-nodejs'
 
 export class TimetableAssistantStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     // The code that defines your stack goes here
-    const timetableAssistantLambda = new lambda.Function(
+    const timetableAssistantLambda = new lambdaNodejs.NodejsFunction(
       this,
       'timetableAssistantLambda',
       {
-        code: lambda.Code.fromAsset('lambda'),
-        handler: 'lambdaHandler.timetableAssistantHandler',
+        entry: 'src/lambda/lambdaHandler.ts',
+        handler: 'tametableAssistantHandler',
         runtime: lambda.Runtime.NODEJS_14_X
       }
     )
 
-    const timetableAssistantApi = new apiGateway.LambdaRestApi(
-      this,
-      'timetableAssistant',
-      {
-        handler: timetableAssistantLambda,
-        proxy: false
-      }
-    )
-
-    const timetableAssistantApiIntegration = new apiGateway.LambdaIntegration(
-      timetableAssistantLambda,
-      {
-        proxy: false,
-        requestParameters: {},
-        requestTemplates: {},
-        passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_MATCH,
-        integrationResponses: [
-          {
-            statusCode: '200',
-            responseTemplates: {
-              'application/json': '$input.json("$")'
-            }
-          }
-        ]
-      }
-    )
-
-    timetableAssistantApi.root
-      .addResource('timetable')
-      .addMethod('GET', timetableAssistantApiIntegration, {
-        requestParameters: {},
-        methodResponses: [{ statusCode: '200' }]
-      })
+    new apiGateway.LambdaRestApi(this, 'timetableAssistantApi', {
+      handler: timetableAssistantLambda
+    })
   }
 }
---
+++ src/lambda/lambdaHandler.ts
@@ -0,0 +1,12 @@
+// import timetable from './timetable'
+
+export const tametableAssistantHandler = () => {
+  // const date = new Date()
+  // const day = `${['日', '月', '火', '水', '木', '金', '土'][date.getDay()]}曜日`
+  // if (day === '日曜日' || day === '土曜日') {
+  //   return timetable['月曜日']
+  // }
+
+  // return { statusCode: 200, body: JSON.stringify(timetable[day]) }
+  return { aho: 'aho' }
+}
